da500db4f97f0398826924074a246c1cbb7cce45
metaxbot <metaxbot@gmail.com>
2025-12-15 15:15:34 +0600

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const config = require("../config.json");
 
 const StatsSchema = new mongoose.Schema({
   totalUsers: { type: Number, default: 0 },
@@ -7,22 +8,45 @@ const StatsSchema = new mongoose.Schema({
 
 const Stats = mongoose.model("Stats", StatsSchema);
 
-let stats = { totalUsers: 0, totalMessages: 0 };
+let stats = {
+  totalUsers: 0,
+  totalMessages: 0
+};
+
 let saveTimeout = null;
 
 async function connectDB() {
-  await mongoose.connect(process.env.MONGO_URI);
+  const uri = process.env.MONGO_URI || config.mongoURI;
+
+  if (!uri) {
+    console.error("❌ MongoDB URI missing!");
+    process.exit(1);
+  }
+
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
   let data = await Stats.findOne();
   if (!data) data = await Stats.create(stats);
+
   stats = data;
   console.log("🗄 MongoDB Connected");
 }
 
 function saveStatsDebounced() {
   clearTimeout(saveTimeout);
-  saveTimeout = setTimeout(() => {
-    Stats.updateOne({}, stats).exec();
+  saveTimeout = setTimeout(async () => {
+    await Stats.updateOne({}, {
+      totalUsers: stats.totalUsers,
+      totalMessages: stats.totalMessages
+    });
   }, 5000);
 }
 
-module.exports = { connectDB, stats, saveStatsDebounced };
+module.exports = {
+  connectDB,
+  stats,
+  saveStatsDebounced
+};
